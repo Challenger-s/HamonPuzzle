5b1317f70e0b169573d2039f2f40962f462a42c6
suzukitaito <denyuki1121@gmail.com>
2020-06-03 03:36:32 +0900

    Merge branch 'taito/work' into NewMaster
    # Conflicts:
    #	Assets/Kairi.meta
    #	Assets/Kairi/Prefabs/Fitzone.prefab
    #	Assets/Kairi/Prefabs/ResonancePoint.prefab
    #	Assets/Kairi/Prefabs/Ripple.prefab
    #	Assets/Kairi/Scenes.meta
    #	Assets/Kairi/Scenes/Kairi_GameScene.unity
    #	Assets/Kairi/Scripts/FitzoneController.cs
    #	Assets/Kairi/Scripts/ResonancePointController.cs
    #	Assets/Kairi/Scripts/RippleController.cs
    #	Assets/Kairi/Scripts/RippleGenerator.cs

---
+++ Assets/Taito File/Scripts/Game/FitzoneChange.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitzoneChange : MonoBehaviour
+{
+    [SerializeField]
+    FitzoneController fitzoneController;
+
+    [SerializeField]
+    GameObject notEnough;
+
+    [SerializeField]
+    SpriteRenderer[] SpNotEnoughs;
+
+    [SerializeField]
+    GameObject complete;
+
+    [SerializeField]
+    GameObject burst;
+
+    int lastCont = 1;
+
+    int clearCont = 0;
+
+    float delta = 0;
+    float span = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clearCont = fitzoneController.GetCount();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!(lastCont == fitzoneController.GetCount()))
+        {
+            lastCont = fitzoneController.GetCount();
+
+            if (fitzoneController.GetCount() == 0)
+            {
+                Complete();
+            }
+            else if(fitzoneController.GetCount() < 0)
+            {
+                Burst();
+            }
+            else if(fitzoneController.GetCount() > 0)
+            {
+                    NotEnough();
+            }
+            
+        }
+
+        
+    }
+
+    void NotEnough()
+    {
+        complete.SetActive(false);
+        burst.SetActive(false);
+        notEnough.SetActive(true);
+
+        if (lastCont < clearCont)
+        {
+            notEnough.transform.localScale = new Vector3(0.6f, 0.6f, 1);
+            for (int i = 0; i < SpNotEnoughs.Length; i++)
+            {
+                SpNotEnoughs[i].color = new Color(SpNotEnoughs[i].color.r, SpNotEnoughs[i].color.g, SpNotEnoughs[i].color.b, SpNotEnoughs[i].color.a - (1 / 3f));
+            }     
+        }
+        else
+        {
+            notEnough.transform.localScale = new Vector3(0.5f, 0.5f, 1);
+            for (int i = 0; i < SpNotEnoughs.Length; i++)
+            {
+                SpNotEnoughs[i].color = new Color(SpNotEnoughs[i].color.r, SpNotEnoughs[i].color.g, SpNotEnoughs[i].color.b, 1);
+            }
+        }
+
+    }
+
+    void Complete()
+    {
+        burst.SetActive(false);
+        notEnough.SetActive(false);
+        complete.SetActive(true);
+        complete.transform.localScale = new Vector3(0.6f, 0.6f, 1);
+    }
+
+    void Burst()
+    {       
+        notEnough.SetActive(false);
+        complete.SetActive(false);
+        burst.SetActive(true);
+    }
+}
---
+++ Assets/Taito File/Shader/Obstacle 1.shader
@@ -0,0 +1,67 @@
+﻿Shader "Obstacle1" {
+	Properties
+	{
+		_MainTex("2D Texture", 2D) = "white" {}
+		_Color("Color", Color) = (1,1,1,1)
+
+	}
+		SubShader
+	{
+		Tags { "RenderType" = "Transparent" "Queue" = "Transparent" }
+
+		Pass
+		{
+		
+			Stencil{
+				Ref 2
+				Comp always
+				Pass replace
+			}
+
+			Tags { "LightMode" = "ForwardBase" }
+
+			ZWrite Off
+
+			CGPROGRAM
+		   #pragma vertex vert
+		   #pragma fragment frag
+
+		   #include "UnityCG.cginc"
+
+			struct appdata
+			{
+				half4 vertex : POSITION;
+				half3 normal : NORMAL;
+			};
+
+			struct v2f
+			{
+				half4 pos : SV_POSITION;
+				half3 normal: TEXCOORD1;
+			};
+
+			half4 _Color;
+			half4 _LightColor0;
+
+			v2f vert(appdata v)
+			{
+				v2f o = (v2f)0;
+
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.normal = UnityObjectToWorldNormal(v.normal);
+
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				//half3 diff = max(0, dot(i.normal, _WorldSpaceLightPos0.xyz)) * _LightColor0;
+
+				fixed4 col;
+				col.rgb = _Color;
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
